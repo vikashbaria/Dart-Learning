58fb0ed3be9d82190fcefcae5b14c78ada7185ee
Vikash Harjeewan <vikashbaria4@gmail.com>
2025-07-16 17:06:10 +0500

    Update Task13.dart

--- Assignment No 2/Task13.dart
+++ Assignment No 2/Task13.dart
@@ -5,7 +5,7 @@ void main() {
   13.  Write a program to calculate the perimeter (circumference) of a
 circle using:*/
 
-  double radius = 7; // You can change the radius value as needed
+  double radius = 7;
 
   double perimeter = 2 * pi * radius;
 
